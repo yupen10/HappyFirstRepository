df31348930fcc737819d067640587f15d914528c
yupen10 <shirai1031@outlook.jp>
2025-05-15 17:10:55 +0900

    なんか流れるやつ改良版

---
+++ sketch_may15b_copy_20250515170844.ino
@@ -0,0 +1,40 @@
+#include<FastLED.h>
+
+#define NUM 30
+int i=0;
+CRGB leds[NUM];
+
+
+void setup() {
+  // put your setup code here, to run once:
+ FastLED.addLeds<WS2812,6>(leds,NUM);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+ i++;
+ leds[i].r=15;
+ leds[i].g=50;
+ leds[i].b=30;
+ 
+ delay(100);
+  
+ leds[i-1].r=0;
+ leds[i-1].g=0;
+ leds[i-1].b=0;
+ delay(100);
+ 
+ if(i>NUM)
+ {
+  i=0;
+
+ }
+ 
+ 
+ 
+ FastLED.show();
+ 
+ 
+ 
+}
